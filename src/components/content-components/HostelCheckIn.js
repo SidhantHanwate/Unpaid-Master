cacfe48e77a554458e8ab3eb82e2477530b72653
Lithesh11 <cs20b036@iittp.ac.in>
2022-09-03 14:48:56 +0530

    balance sheet

--- src/components/content-components/HostelCheckIn.js
+++ src/components/content-components/HostelCheckIn.js
@@ -1,7 +1,38 @@
-import React from 'react'
+import React from "react";
+
+import Style from "../modules/HostelCheckIn.module.css";
 
 export function HostelCheckIn() {
   return (
-    <div>HostelCheckIn</div>
-  )
+    <div className="CheckIn" id="login">
+      <div className="header">HostelCheckIn</div>
+      <div className="Form">
+        <form>
+          <div>
+            <label htmlFor="rollnoin">Roll No</label>
+            <input
+              type="text"
+              required
+              placeholder="Enter Roll No"
+              name="rollnoin"
+            />
+          </div>
+          <div>
+            <label htmlFor="namein">Name</label>
+            <input
+              type="text"
+              required
+              placeholder="Enter name"
+              name="namein"
+            />
+          </div>
+
+          <div className="date">{/* <p> ddd {date}</p> */}</div>
+          <div>
+            <button type="submit">Submit</button>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
 }
