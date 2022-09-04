49363e22ea9c20764ba9e2b241a742a573d2fa50
cs20b041 <cs20b041@iittp.ac.in>
2022-09-05 02:21:31 +0530

    Small changes

--- src/components/content-components/HostelCheckIn.js
+++ src/components/content-components/HostelCheckIn.js
@@ -1,5 +1,70 @@
 import React from "react";
 
-export const HostelCheckIn = () => {
-	return <div>HostelCheckIn</div>;
-};
+import Style from "../modules/HostelCheckIn.module.css";
+
+export function HostelCheckIn() {
+	return (
+		<div className="CheckIn" id="login">
+			<div className="header">HostelCheckIn</div>
+			<div className="Form">
+				<form>
+					<div>
+						<label htmlFor="rollnoin">Roll No</label>
+						<input
+							type="text"
+							required
+							placeholder="Enter Roll No"
+							name="rollnoin"
+						/>
+					</div>
+					<div>
+						<label htmlFor="namein">Name</label>
+						<input
+							type="text"
+							required
+							placeholder="Enter name"
+							name="namein"
+						/>
+					</div>
+
+					<div className="date">{/* <p> ddd {this.state.date}</p> */}</div>
+					<div>
+						<button type="submit">Submit</button>
+					</div>
+				</form>
+			</div>
+		</div>
+	);
+	return (
+		<div className="CheckIn" id="login">
+			<div className="header">HostelCheckIn</div>
+			<div className="Form">
+				<form>
+					<div>
+						<label htmlFor="rollnoin">Roll No</label>
+						<input
+							type="text"
+							required
+							placeholder="Enter Roll No"
+							name="rollnoin"
+						/>
+					</div>
+					<div>
+						<label htmlFor="namein">Name</label>
+						<input
+							type="text"
+							required
+							placeholder="Enter name"
+							name="namein"
+						/>
+					</div>
+
+					<div className="date">{/* <p> ddd {date}</p> */}</div>
+					<div>
+						<button type="submit">Submit</button>
+					</div>
+				</form>
+			</div>
+		</div>
+	);
+}
