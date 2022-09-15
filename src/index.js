d94151dd992f47247ae55b7a8063b31ed0a45a9c
cs20b041 <cs20b041@iittp.ac.in>
2022-09-15 19:15:59 +0530

    Added LOC pdf and Budget Calculator

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import React from "react";
-import ReactDOM from "react-dom/client";
+import ReactDOM from "react-dom";
 import "./index.css";
 import reportWebVitals from "./reportWebVitals";
+// Added ReactDOM.render(<App/>, document.getElementById("root"));
 
 import { Home } from "./components/Home";
 import { Confirmation } from "./components/Confirmation";
@@ -13,7 +14,7 @@ import { Profile } from "./components/Profile";
 
 import { BalanceSheetGen } from "./components/content-components/BalanceSheetGen";
 import { IncomeSheetGen } from "./components/content-components/IncomeSheetGen";
-import { Budget } from "./components/content-components/Budget";
+//import { BudgetCal } from "./components/content-components/Budget";
 
 import { Ledgers } from "./components/content-components/Ledgers";
 import { MISReports } from "./components/content-components/MISReports";
@@ -35,59 +36,60 @@ import { ContentHome } from "./components/content-components/ContentHome";
 import { NoProfile } from "./components/NoProfile";
 
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import BudgetCalculator from "./components/content-components/Budget";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <BrowserRouter basename="/">
-    <Routes>
-      <Route path="/" element={<Home />} />
+	<BrowserRouter basename="/">
+		<Routes>
+			<Route path="/" element={<Home />} />
 
-      <Route path="/home" element={<Home />}>
-        <Route path="financial-reports">
-          <Route path="balance-sheet-gen" element={<BalanceSheetGen />} />
-          <Route path="income-sheet-gen" element={<IncomeSheetGen />} />
-          <Route path="budget" element={<Budget />} />
-        </Route>
+			<Route path="/home" element={<Home />}>
+				<Route path="financial-reports">
+					<Route path="balance-sheet-gen" element={<BalanceSheetGen />} />
+					<Route path="income-sheet-gen" element={<IncomeSheetGen />} />
+					<Route path="budget" element={<BudgetCalculator />} />
+				</Route>
 
-        <Route path="transaction-management">
-          <Route path="ledger" element={<Ledgers />} />
-          <Route path="mis-reports" element={<MISReports />} />
-          <Route path="hefa-manage" element={<HEFARequest />} />
-          <Route path="letter-of-credit" element={<LetterOfCredit />} />
-          <Route path="emd-showcase" element={<EMDShowCase />} />
-        </Route>
+				<Route path="transaction-management">
+					<Route path="ledger" element={<Ledgers />} />
+					<Route path="mis-reports" element={<MISReports />} />
+					<Route path="hefa-manage" element={<HEFARequest />} />
+					<Route path="letter-of-credit" element={<LetterOfCredit />} />
+					<Route path="emd-showcase" element={<EMDShowCase />} />
+				</Route>
 
-        <Route path="faculty">
-          <Route path="payroll-faculty" element={<FacultyPayr />} />
-          <Route path="payroll-staff" element={<StaffPayr />} />
-          <Route path="payroll-adhoc" element={<AdHocPayr />} />
-        </Route>
+				<Route path="faculty">
+					<Route path="payroll-faculty" element={<FacultyPayr />} />
+					<Route path="payroll-staff" element={<StaffPayr />} />
+					<Route path="payroll-adhoc" element={<AdHocPayr />} />
+				</Route>
 
-        <Route path="students-portal">
-          <Route path="hostel-feechallan" element={<HostelFeeChallan />} />
-          <Route path="hostel-checkout" element={<HostelCheckOut />} />
-          <Route path="hostel-checkin" element={<HostelCheckIn />} />
-          <Route path="payment-portal" element={<PaymentPortal />} />
-          <Route path="fee-management" element={<FeeManagement />} />
-        </Route>
+				<Route path="students-portal">
+					<Route path="hostel-feechallan" element={<HostelFeeChallan />} />
+					<Route path="hostel-checkout" element={<HostelCheckOut />} />
+					<Route path="hostel-checkin" element={<HostelCheckIn />} />
+					<Route path="payment-portal" element={<PaymentPortal />} />
+					<Route path="fee-management" element={<FeeManagement />} />
+				</Route>
 
-        <Route index element={<ContentHome />} />
-        <Route path="content-home" element={<ContentHome />} />
-      </Route>
+				<Route index element={<ContentHome />} />
+				<Route path="content-home" element={<ContentHome />} />
+			</Route>
 
-      <Route path="/profile" element={<NoProfile />} />
-      <Route path="/profile/:uniqueid" element={<Profile />} />
+			<Route path="/profile" element={<NoProfile />} />
+			<Route path="/profile/:uniqueid" element={<Profile />} />
 
-      <Route path="/confirmation/:details" element={<Confirmation />} />
+			<Route path="/confirmation/:details" element={<Confirmation />} />
 
-      <Route path="/login" element={<Login />}>
-        <Route path="student" element={<LoginStudent />} />
-        <Route path="faculty" element={<LoginFaculty />} />
-      </Route>
+			<Route path="/login" element={<Login />}>
+				<Route path="student" element={<LoginStudent />} />
+				<Route path="faculty" element={<LoginFaculty />} />
+			</Route>
 
-      <Route path="*" element={<PageNotFound />} />
-    </Routes>
-  </BrowserRouter>
+			<Route path="*" element={<PageNotFound />} />
+		</Routes>
+	</BrowserRouter>
 );
 
 // If you want to start measuring performance in your app, pass a function
